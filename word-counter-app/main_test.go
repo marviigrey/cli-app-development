d4e223a0fda96d1037c4c20ee2ee75a42644f4ba
marviigrey <marvellousfrederick@gmail.com>
2024-01-19 01:13:00 +0100

    test updated

--- word-counter-app/main_test.go
+++ word-counter-app/main_test.go
@@ -5,35 +5,12 @@ import (
 	"testing"
 )
 
-func TestCountWords(t *testing.T) {
-	// Declared a variable to help read an existing string using the newBufferString function.
-	b := bytes.NewBufferString("word1 word2 word3 word4\n")
-
-	//a variable to define the result of the count(b) function.
-	res := count(b, false, false)
-	expected := 4
-	//if we fail to get then number declared in the "expected" it will return an error.
-	if expected != res {
-		t.Errorf("expected: %v, got: %v", expected, res)
-	}
-}
-
-func testCountLines(t *testing.T) {
-	b := bytes.NewBufferString("word1 word2\nline2 \nline1word3 word4")
+func testCountWords(t *testing.T) {
+	words := "i love you"
+	b := bytes.NewBufferString(words)
 	res := count(b, false, false)
 	exp := 3
-
-	if exp != res {
-		t.Errorf("expected: %v, got: %v", exp, res)
-	}
-
-}
-
-func testCountBytes(t *testing.T) {
-	b := bytes.NewBufferString("word1")
-	res := count(b, false, false)
-	exp := 4
-	if exp != res {
-		t.Errorf("expected: %v, got: %v", exp, res)
+	if res != exp {
+		t.Errorf("expected %v, got: %v", exp, res)
 	}
 }
